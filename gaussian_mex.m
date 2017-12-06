d5f1aaa152a860e24557a79e5587e12c59826e52
BLANCHOUD Simon <simon.blanchoud@unifr.ch>
2017-12-06 23:40:56 +0100

    Wrong MEX

--- gaussian_mex.m
+++
@@ -1,8 +0,0 @@
-% GAUSSIAN_MEX gaussian smoothing in C for speedup. It utilizes the code from Anthony
-% Gabrielson to do the actual computations (see MEX/gaussian_smooth.c).
-%
-%   GAU = GAUSSIAN_MEX(IMG, SIGMA) applies a gaussian filtering with a SIGMA kernal.
-%
-% Gonczy & Naef labs, EPFL
-% Simon Blanchoud
-% 16.05.2014
